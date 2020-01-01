becf6e38c6dbfea63dbd3633883fd33327faff39
aagundez <aagundez@google.com>
2020-01-01 15:50:37 -0800

    Fix paths

--- scripts/pull-wkt.sh
+++ scripts/pull-wkt.sh
@@ -2,16 +2,18 @@
 
 # Pulls protobuf's well-known types from Google's GitHub repo.
 
-rm -rf ./protos/*
-tmp=./tmp_pull
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+rm -rf $SCRIPT_PATH/../protos/*
+tmp=$SCRIPT_PATH/.tmp_pull
 git init $tmp
 cd $tmp
 git remote add origin https://github.com/protocolbuffers/protobuf.git
 git config core.sparsecheckout true
 echo "src/google/protobuf/*.proto" >> .git/info/sparse-checkout
 git pull --depth=1 origin master
-cd ..
-mkdir -p ./protos/google/protobuf/
-cp -r $tmp/src/google/protobuf/ ./protos/google/protobuf/
-rm -rf ./protos/google/protobuf/*{test,map_}*
+cd $SCRIPT_PATH
+mkdir -p $SCRIPT_PATH/../protos/google/protobuf/
+cp -r $tmp/src/google $SCRIPT_PATH/../protos
+rm -rf $SCRIPT_PATH/../protos/google/protobuf/*{test,map_}*
 rm -rf $tmp
